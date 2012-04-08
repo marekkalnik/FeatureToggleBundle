4edc177bbae73a6c78a11aa7f0fab10139c62fbf
Benjamin Grandfond <benjamin.grandfond@gmail.com>
2012-04-09 00:25:54 +0200

    Rename EmkaFeatureToggleExtension into FeatureToogleExtension.

--- DependencyInjection/FeatureToggleExtension.php
+++ DependencyInjection/FeatureToggleExtension.php
@@ -4,17 +4,18 @@ namespace Emka\FeatureToggleBundle\DependencyInjection;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\Definition\Processor;
+
 /**
- * 
+ *
  * @author Marek Kalnik <marekk@theodo.fr>
  * @since  2011-09-20
  */
-class EmkaFeatureToggleExtension extends Extension
+class FeatureToggleExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container)
     {
-    	$processor = new Processor();
-    	$configuration = new Configuration();
-    	$config = $processor->processConfiguration($configuration, $configs);
+        $processor     = new Processor();
+        $configuration = new Configuration();
+        $config        = $processor->processConfiguration($configuration, $configs);
     }
 }
